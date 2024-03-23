234aade5670c0df52ee7ac63b4dd0ed218c8f9bd
Ayrus707 <122613157+Ayrus707@users.noreply.github.com>
2024-03-23 16:12:06 +0000

    datatypes done

---
+++ thebasics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict" //all the code will be treated as a newer version of js
+
+//alert(4+3) , it won't work here with nodejs but it will work when we will use the broswer-embedded-js
+
+let name="surya" //dt:string
+let age=18       //dt: int
+let ismale=true  //dt:boolean
+
+//About Datatypes->
+/* [primitive datatypes]
+number-> range is (2^53)
+bigInt-> has more range as compared to number
+string-> usual representation is in ""(double quotes)
+boolean->stores either true or false
+null-> to represent empty value(standalone value)
+undefined-> meaning that no value is defined for the given datatype/variable
+symbol-> to identify uniqueness
+*/
+
+//objects
+
+//usage of typeof
+console.log(typeof undefined) // note: undefined has it's own type 
+console.log(typeof null) // note: null does not have it's own type rather it's considered as an object in js. 
