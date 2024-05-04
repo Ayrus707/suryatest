b3cd5e8a1c2723a0e05cca1457933a280d1f0b0b
Ayrus707 <122613157+Ayrus707@users.noreply.github.com>
2024-05-04 15:38:33 +0000

    basics_02 created

---
+++ thebasics_02/arrays_01.js
@@ -0,0 +1,6 @@
+function myarray(){
+    const arr=[0,1,2,3,4,"surya","shrma"] //Declaring array, the array in Js can contain hetrogenous elements.
+    console.log(arr[7]); //out of index is undefined, doesn't give any errors.
+    console.log(arr.length); //length method is used to Find the lenngth of the array in js.
+}
+myarray()
